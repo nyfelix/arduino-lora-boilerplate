50987d0275748053433173dd29443f1bec7bde64
nyfelix <nyfelix@gmail.com>
2020-04-12 15:47:20 +0200

    Change to Cayenne LPP

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 
 /* System behavior */
 //#define DEEPSLEEP // Use Deepsleep (if not, it sleep will be simulated)
-#define SEND_INTERVAL 30   // Time in seconds
+#define SEND_INTERVAL 10   // Time in seconds
 #define MEASURE_INTERVAL 2 // Time in seconds
 
 /* LoRa Settings */
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <lmic_util.h>
 #include <Adafruit_SleepyDog.h>
 #include <avr/pgmspace.h>
-
+#include <CayenneLPP.h>
 
 //******************Configuration of TinyLoRa********************
 uint8_t NwkSkey[16] = NWKSKEY;
@@ -19,89 +19,61 @@ uint8_t DevAddr[4] = DEVADDR;
 TinyLoRa lora = TinyLoRa(DIO1, NSS, RST);
 
 // Buffer for the LoRa payload.
-unsigned char payload[12];
+CayenneLPP payload(12);
 
 int sendInterval = SEND_INTERVAL; // duration of watchdochg sleeptime in s. Minimal sleepduration is 30s
 int measureInverval = MEASURE_INTERVAL;
 int sleepcounter = 0; // counts the how many times the watchdogSleep or simulateSleep reseted the counter.
+int sendCount = sendInterval/measureInverval;
 volatile bool sleepbit=false; //first loop without sleeping
 
-/*
-  Converts a float variable to a 2 byte int and ads it to the payload buffer. The Value needs to be decoded in the TTN decoder. 
-*/
-void floatToPayload(uint8_t i, float value ) {
-  // float -> int
-  uint16_t payloadValue = LMIC_f2sflt16(value);
-  // int -> bytes
-  byte low = lowByte(payloadValue);
-  byte high = highByte(payloadValue);
-  // place the bytes into the payload
-  payload[i] = low;
-  payload[i+1] = high;
-}
 
-void intToPayLoad(uint8_t i, int value) {
-  byte low = lowByte(value);
-  byte high = highByte(value);
-  // place the bytes into the payload
-  payload[i] = low;
-  payload[i+1] = high;
-}
-
-/* Prints the payload. Used for debugging. */
-void print_payload(){
-  debug("Payload: ");
-  for(unsigned int i=0; i<sizeof(payload);i++){
-    debug(payload[i], HEX);
-  debug(" ");
-  }
-  debugLn();
-}
 
 /* Encode the data to send to the byte array payload. */
 void  preparePayolad() {
+  payload.reset();
   float temperature = 25;
-  intToPayLoad(0, temperature);
+  payload.addTemperature(1,temperature);
   float humidity = 0.8;
-  floatToPayload(2, humidity);
+  payload.addRelativeHumidity(2, humidity);
   // only works on Feather boards
   float vbat = analogRead(VBATPIN);
   vbat *= 2;    // we divided by 2, so multiply back
   vbat *= REFVOL;  // Multiply by 3.3V, our reference voltage
   vbat /= 1024; // convert to voltage
   vbat /= 10;
-  floatToPayload(4, vbat);
+  payload.addVoltage(3, vbat);
 }
 
 /* Prepare a simple state machine*/
 enum States{
   observing,
-  sending
+  sending,
+  finished
 };
 
 States currState = observing;
 
 /* This function is used to set the watchdog timer multipel times, so the sleep duration is longer than the max. value of the watchdogtimer.*/
-void watchdogSleep(int time_s, volatile bool*sleepflag){
-  
-  double sleep_rep=time_s/30;
-  while (*sleepflag==true){
-    Watchdog.sleep(30000);//sleeptime in ms  
-    sleepcounter++;
-    if(sleepcounter>=sleep_rep){
-      *sleepflag=false;    
-      sleepcounter=0; // reset the sleepcounter
-    }
-  }
-  *sleepflag=true;// reset sleepbit
+void watchdogSleep(int time_s){
+  sleepbit = true;
+  Watchdog.sleep(time_s*1000);//sleeptime in ms  
+  sleepcounter++;
+  sleepbit = false;
 }
 
 /* This function simulates sleep. But the mikrocontroller doesn't enter sleepmode, so the serial communication doesn't break down.*/
-void simulateSleep(int time_s, volatile bool*sleepflag){
+void simulateSleep(int time_s){
   // ToDo: Simulate Interrupt
+  sleepbit = true;
   delay(time_s*1000);
+  sleepcounter++;
+  sleepbit = false;
 }
 
+void measure() {
+  
+}
 
 void setup(){  
   
@@ -128,37 +100,34 @@ void loop()
   switch (currState) { 
     
     case observing:{ // u gathering and processing information with sleep pauses
-      debugLn("observing...");
-      
-      digitalWrite(LED_BUILTIN,LOW);
+      debugLn("measure...");
+      measure();
+      debugLn("sleep");
       #ifndef DEEPSLEEP
-        simulateSleep(sendInterval, &sleepbit);
+        simulateSleep(sendInterval);
+      #else 
+        watchdogSleep(measureInverval);
       #endif
-      #ifdef DEEPSLEEP 
-        watchdogSleep(measureInverval, &sleepbit);
-      #endif
-      digitalWrite(LED_BUILTIN,HIGH);
       debugLn("end of sleep");
       // Read sensor data.,
-      
-      currState=sending;
+      if (sleepcounter >= sendCount) {
+        currState=sending;
+      }
       break; 
     }
       
     case sending:{ 
       digitalWrite(LED_BUILTIN, HIGH);
       debugLn("Sending LoRa Data...");
-      preparePayolad();
-      #ifdef DEBUG
-        print_payload();
-      #endif
       debug("Frame Counter: "); 
       debugLn(lora.frameCounter);
-      lora.sendData(payload, sizeof(payload), lora.frameCounter);  
+      preparePayolad();
+      //lora.sendData(payload.getBuffer(), payload.getSize() , lora.frameCounter);  
       lora.frameCounter++;
       delay(1000);
       digitalWrite(LED_BUILTIN, LOW);
       currState=observing;
+      //currState=finished;
       break;
     } 
   }       
